bea41c324cb06508e5675393a9f34b36bb262cc1
siwoo <k74415834@gmail.com>
2025-03-04 22:26:11 +0900

    test application yml 생성 테스트

--- src/test/java/com/example/demoApplication/DemoApplicationTests.java
+++ src/test/java/com/example/demoApplication/DemoApplicationTests.java
@@ -2,10 +2,14 @@ package com.example.demoApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests extends UsingTestContainerTest {
+
+	@Value("${my.name}")
+	String yes;
 
 	@Autowired
 	TestForTest testForTest;
@@ -15,6 +19,7 @@ class DemoApplicationTests extends UsingTestContainerTest {
 		testForTest.successTest();
 		testForTest.repoTest();
 
+		System.out.println("yes: " + yes);
 	}
 
 
